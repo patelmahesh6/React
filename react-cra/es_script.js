1b5a8b17f539e9a73335e213830be5964d6d861f
INTER.USAMA <12345678>
2019-02-05 07:52:00 +0400

    Es tags

---
+++ react-cra/es_script.js
@@ -0,0 +1,13 @@
+// cl
+console.log("es 6 syntax");
+
+// c  constant / Scope Block
+const fname = "Mahesh";
+
+// l  let can be updated but not re-declared  / Scope Block
+let lname = "Patel";
+console.log(lname);
+
+// v  variable   / Scope Function
+var fullName = fname + lname;
+console.warn(fullName);
